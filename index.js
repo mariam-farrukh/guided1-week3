c69be8e6d198a2f00d182bd7a011bfe7a608d150
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-16 12:43:00 -0500

    Middleware restricted

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -58,6 +58,28 @@ server.get('/hash', (req, res) => {
   const has = ncrypt.hashSync(name, 14);
   res.send(`the hash for ${name} is ${hash}`)
 });
+
+// Middleware
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+      Users.findBy({ username })
+          .first()
+          .then(user => {
+              if (user && bcrypt.compareSync(password, user.password)) {
+                  next();
+              } else {
+                  res.status(401).json({ message: 'Invalid Credentials' });
+              }
+          })
+          .catch(error => {
+              res.status(500).json({ message: 'Unexpected error' });
+          });
+  } else {
+      res.status(400).json({ message: 'No credentials provided' });
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
